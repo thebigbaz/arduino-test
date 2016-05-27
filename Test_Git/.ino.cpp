30f33f04e6da6bcab31082a17cf3fce595149794
Jason Baziliauskas <limpet@white-tigers-elite.co.uk>
2016-05-27 13:48:37 +0100

    Importing in a test update

--- Test_Git/.ino.cpp
+++ Test_Git/.ino.cpp
@@ -2,12 +2,13 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2016-05-25 10:46:38
+//This file has been generated on 2016-05-27 10:52:45
 
 #include "Arduino.h"
 #include "Arduino.h"
 void setup() ;
 void loop() ;
+void triggerAlarm();
 
 #include "Test_Git.ino"
 
--- Test_Git/Test_Git.ino
+++ Test_Git/Test_Git.ino
@@ -1,21 +1,71 @@
 #include "Arduino.h"
+
 //The setup unction is called once at startup of the sketch
 
-bool bollox(){
-	return (true);
-}
+int zReading = A0;
+
+bool redWireState = true;
+bool purpleWireState = true;
+
+void triggerAlarm();
 
 void setup()
 {
 // Add your initialization code here
+	Serial.begin(9600);
+	pinMode(D8,OUTPUT);
+	pinMode(D6, INPUT_PULLUP);
+	pinMode(D7, INPUT_PULLUP);
+    redWireState = true;
+    purpleWireState = true;
+
 }
 
 // The loop function is called in an endless loop
 void loop()
 {
+
+	int zPoint = analogRead(zReading);
+    if (purpleWireState == true && digitalRead(D6) == HIGH){
+    	purpleWireState = false;
+    }
+
+    if (redWireState == true && digitalRead(D7) == HIGH && purpleWireState == true){
+    	redWireState = false;
+    }
+
+    if (redWireState == true && purpleWireState == false){
+    	triggerAlarm();
+    	purpleWireState = true;
+    }
+
+    zPoint = analogRead(zReading);
 //Add your repeated code here
-	while(bollox())
+    if (redWireState == true )
+    {
+		while( ((zPoint < 340) || (zPoint > 600 ))  )
+		{
+			Serial.print("X triggered = ");
+			Serial.println(zPoint);
+
+			triggerAlarm();
+
+			zPoint = analogRead(zReading);
+
+		}
+    }
+	Serial.print("X = ");
+	Serial.println(zPoint);
+	delay(100);
+}
+
+
+void triggerAlarm(){
+	for(int i = 0; i < 10;i++)
 	{
-		delay(20);
+		digitalWrite(D8,HIGH);
+		delay(500);
+		digitalWrite(D8,LOW);
+		delay(500);
 	}
 }
